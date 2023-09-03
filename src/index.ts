9887082e26757352dc3d23cae550451ed1094ddc
Draghici Rares-Valentin <draghici.rares212@gmail.com>
2023-09-03 21:48:18 +0300

    add auth0 integration | fix dashboard page scrolling

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,13 @@ export * from './lib/domain/printing/util/print-quality.enum';
 export * from './lib/domain/printing/util/print-strength.enum';
 export * from './lib/domain/printing/util/supported-file-types.enum';
 export * from './lib/domain/printing/models/print-cost-part.dto';
+
 export * from './lib/domain/shop/models/shop-item.dto';
+
 export * from './lib/domain/common/models/paginated-request.dto';
 export * from './lib/domain/common/models/filter.dto';
 export * from './lib/domain/common/models/file-info.dto';
 export * from './lib/domain/common/models/dictionary-value.dto';
 export * from './lib/domain/common/const/dictionary-keys.enum';
+
+export * from './lib/domain/auth/models/haus-user.dto';
